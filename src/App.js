3a56010771f280a78a63849a586b77d445d675c9
Deepak Mahakale <deepakmahakale@gmail.com>
2018-04-13 19:56:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>React changed here!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
